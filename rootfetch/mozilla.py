b596d905ed7a89779dfec7dbdbd1e9501ac140f5
Zakir Durumeric <zakird@gmail.com>
2016-09-01 14:23:18 -0700

    mozilla works

--- rootfetch/mozilla.py
+++ rootfetch/mozilla.py
@@ -7,11 +7,12 @@ class MozillaFetcher(RootStoreFetcher):
     MOZILLA_URL = "https://hg.mozilla.org/mozilla-central/raw-file/tip/security/nss/lib/ckfw/builtins/certdata.txt"
 
     def fetch(self, output):
-
         raw_path = self._make_temp_path("rootfetch-mozilla-raw")
         urllib.urlretrieve(self.MOZILLA_URL, raw_path)
         cabe_path = self._make_temp_path("rootfetch-microsoft-cab-extracted")
-        subprocess.check_call("cabextract -p %s > %s" % (cab_path, cabe_path), shell=True)
+        subprocess.check_call("extract-nss-root-certs %s > %s" % (raw_path, cabe_path), shell=True)
+        with open(cabe_path) as fd:
+            output.write(fd.read())
 
 
 if __name__ == "__main__":
