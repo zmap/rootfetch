1bc94bb75cf3bb351b890916826760e6ed06151b
bwireman <ben@censys.io>
2020-10-09 12:23:24 -0400

    updates google ct RootStoreFetcher to use a union of the argon2023, xenon2023, & pilot logs

--- rootfetch/ct.py
+++ rootfetch/ct.py
@@ -32,6 +32,36 @@ class CTFetcher(RootStoreFetcher):
             output.flush()
 
 
+class CTUnionFetcher(RootStoreFetcher):
+    """CTUnionFetcher fetches the root store from multiple Certificate Transparency logs and returns their union.
+
+    The CT log servers must comply to V1 of the CT API (RFC 6962). Subclass to
+    define the log server URL."""
+
+    URLS = list()
+
+    def make_pem(self, raw):
+        yield "-----BEGIN CERTIFICATE-----"
+        for l in self.split(raw, 64):
+            yield l
+        yield "-----END CERTIFICATE-----"
+
+    def get(self, url):
+        return json.loads(urllib2.urlopen(url).read())
+
+    def fetch(self, output):
+        pems = set()
+        for url in self.URLS:
+            print(url)
+            for certificate in self.get(url)["certificates"]:
+                pems.add("\n".join(self.make_pem(certificate)))
+
+        for pem in pems:
+            output.write(pem)
+            output.write("\n")
+            output.flush()
+
+
 class GoogleAviator(CTFetcher):
 
     URL = "https://ct.googleapis.com/aviator/ct/v1/get-roots"
@@ -57,7 +87,25 @@ class GoogleSkydiver(CTFetcher):
     URL = "https://ct.googleapis.com/skydiver/ct/v1/get-roots"
 
 
+class GoogleXenon2023(CTFetcher):
+
+    URL = "https://ct.googleapis.com/logs/xenon2023/ct/v1/get-roots"
+
+
+class GoogleArgon2023(CTFetcher):
+
+    URL = "https://ct.googleapis.com/logs/argon2023/ct/v1/get-roots"
+
+
+class GoogleArgon2023UnionXenonUnionPilot(CTUnionFetcher):
+
+    URLS = ["https://ct.googleapis.com/logs/argon2023/ct/v1/get-roots",
+            "https://ct.googleapis.com/logs/xenon2023/ct/v1/get-roots",
+            "https://ct.googleapis.com/pilot/ct/v1/get-roots"
+    ]
+
+
 if __name__ == "__main__":
-    m = GooglePilot()
+    m = GoogleArgon2023UnionXenonUnionPilot()
     m.setup()
     m.fetch(sys.stdout)
